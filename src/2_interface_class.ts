1b0bad62251c2a76ea60cda9e51757c15d7eec16
Mark Lee <mark@protopie.io>
2020-12-07 00:30:47 +0900

    3번 문제 업로드

--- src/2_interface_class.ts
+++ src/2_interface_class.ts
@@ -1,1 +1,5 @@
-console.log('question2.js');
+/*
+
+
+
+*/
--- src/3_type_guards.ts
+++ src/3_type_guards.ts
@@ -1,1 +1,29 @@
-console.log('question3.js');
+/*
+Toast 라는 타입은 모두 type 이라는 프로퍼티를 가지고 type 은 다음과 같은 것으로 분류할 수 있다.
+"AFTER_SAVED" | "AFTER_PUBLISHED" | "AFTER_RESTORE"
+*/
+
+enum ToastType {
+  AFTER_SAVED,
+  AFTER_PUBLISHED,
+  AFTER_RESTORE,
+}
+
+interface Toast {
+  type: ToastType;
+  createdAt: string;
+}
+
+const toasts: Toast[] = [];
+
+/*
+toasts 안에 여러개의 toast 가 있습니다.
+toasts 를 인자로 받아서 각각의 toast 를 문자열로 변환하는 함수를 작성합니다.
+
+각각의 toast 를 문자열로 리턴하는 것으로 가정하고, 해당 로직을 작성하세요.
+toasts 를 돌며 toast 를 꺼내서 toast 의 타입이 AFTER_PUBLISHED 인 경우, "퍼블리시가 된 직후에 뜬다!!" 라고 문자열을 만들어냅니다.
+
+해당 함수를 안전하게 완성해주세요 (ToastType 이 추가된 경우에도 안전하도록 작성해보세요.)
+*/
+
+function convert(toasts: Toast[]): string[] {}
